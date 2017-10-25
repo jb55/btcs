73c07bcb6ee2641f2243afd27ab44a022f88fff9
William Casarin <jb55@jb55.com>
2017-10-25 11:42:06 -0700

    fixup! mph-opcodes: generation warning

--- oplookup.h
+++ oplookup.h
@@ -1,3 +1,10 @@
+/* THIS IS GENERATED FROM mph-opcodes DO NOT EDIT */
+
+#ifndef BCS_OPLOOKUP_H
+#define BCS_OPLOOKUP_H
+
 #include "misc.h"
 extern const int opcodes_g[120];
 extern const int opcodes_v[120];
+
+#endif /* BCS_OPLOOKUP_H */
